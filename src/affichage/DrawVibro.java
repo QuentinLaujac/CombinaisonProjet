d88ee59643aac2d94149e4ff7e5600b770084aa8
dauchy <dauchy@a16p6.fil.univ-lille1.fr.>
2015-02-09 11:37:56 +0100

    Ajouts points graphique.

---
+++ src/affichage/DrawVibro.java
@@ -0,0 +1,20 @@
+package affichage;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class DrawVibro extends JPanel{
+
+	public DrawVibro() {
+		setPreferredSize(new Dimension(100, 100));
+	}
+
+	public void drawCircle(Graphics cg, int xCenter, int yCenter, int r) {
+		cg.setPaintMode();
+		cg.setColor(new Color(0,255,255,127));
+		cg.fillOval(xCenter-r, yCenter-r, 3*r, 3*r);
+	}
+}
--- src/affichage/FenetrePrincipale.java
+++ src/affichage/FenetrePrincipale.java
@@ -2,25 +2,26 @@ package affichage;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import metier.EnumCombinaison;
-
-public class FenetrePrincipale extends JFrame {
+public class FenetrePrincipale extends JFrame implements MouseListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private DrawVibro vibro = new DrawVibro();
 
 	public FenetrePrincipale() {
 		super();
 
-		this.setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
 
 		JPanel panelCenter = new JPanel();
 		panelCenter.setLayout(new GridLayout(1, 2));
@@ -28,21 +29,60 @@ public class FenetrePrincipale extends JFrame {
 		JPanel panelFace = new JPanel();
 		panelFace.setLayout(new GridLayout(20, 20));
 
-		for (int i = 0; i != 20; i++) {
+		JPanel panelDos = new JPanel();
+		panelDos.setLayout(new GridLayout(20, 20));
+
+		/*for (int i = 0; i != 20; i++) {
 			for (int j = 0; j != 20; j++) {
 				panelFace.add(new BoutonMot(EnumCombinaison.RIEN));
 			}
-		}
+		}*/
 
 		JLabel labFace = new JLabel(new ImageIcon("face.jpg"));
 		labFace.setBounds(0, 0, 270, 230);
-		panelCenter.add(panelFace);
 		JLabel labDos = new JLabel(new ImageIcon("dos.jpg"));
 		labDos.setBounds(0, 0, 270, 230);
+		panelFace.add(labFace);
+		panelDos.add(labDos);
+
+		panelCenter.add(labFace);
 		panelCenter.add(labDos);
 		this.add(panelCenter, BorderLayout.CENTER);
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panelCenter.addMouseListener(this);
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println(e.getLocationOnScreen());
+		DrawVibro drawVibro = new DrawVibro();
+		drawVibro.drawCircle(getGraphics(), e.getX(), e.getY()+35, 10);
+		this.add(new DrawVibro());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 
 	}
 
